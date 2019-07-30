ec3379798d907627a433a8920a37c49a21c4d529
Dev-Bobbie <bobbie_liu88@163.com>
2019-07-30 21:34:03 +0800

    update

---
+++ chapter13/__init__.py
@@ -0,0 +1,11 @@
+"user.persAcctNo":"520851792162",
+"user.uidentifyno":"dda8a6b9007c6d23d06a11c815aaa9df",
+"user.uname":"(unable to decode value)",
+"user.password":"6205a1e54830fb9aa93f4761aa9328b3",
+"validateno":"hhhg",
+
+
+
+
+职工账号: 520851792162, 查询密码: asd123000, 职工姓名: 郭辉, 身份证号: 372929198711022112
+职工账号: 512117538450, 查询密码: 198325, 职工姓名: 朱乾林, 身份证号: 220102198303190012
